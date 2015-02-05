7120bad0599d4aacd489669db2e4650f1a3eb4a3
tiger008 <mythom008@gmail.com>
2015-02-06 00:32:54 +0100

    Norme IG TP1

--- POnGL/TP1/NReines/IG/ZoneCliquable.java
+++ POnGL/TP1/NReines/IG/ZoneCliquable.java
@@ -8,39 +8,46 @@ import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public abstract class ZoneCliquable extends JPanel implements MouseListener {
+public abstract class ZoneCliquable extends JPanel implements MouseListener
+{
+	private Texte texte;
 
-    private Texte texte;
-
-    public ZoneCliquable(int x, int y) {
-	setPreferredSize(new Dimension(x, y));
-	addMouseListener(this);
-	setBackground(Color.WHITE);
-    }
-    
-    public ZoneCliquable(String texte, int x, int y) {
-	this(x, y);
-	this.texte = new Texte(texte);
-	this.add(this.texte);
-    }
-
-    public void changeTexte(String texte) {
-	this.texte.changeTexte(texte);
-    }
-    
-    protected abstract void clicGauche();
-    protected abstract void clicDroit();
-    
-    public void mouseClicked(MouseEvent e) {
-	if (SwingUtilities.isRightMouseButton(e)) {
-	    this.clicDroit();
-	} else {
-	    this.clicGauche();
+	public ZoneCliquable(int x, int y)
+	{
+		setPreferredSize(new Dimension(x, y));
+		addMouseListener(this);
+		setBackground(Color.WHITE);
 	}
-    }
 
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
+	public ZoneCliquable(String texte, int x, int y)
+	{
+		this(x, y);
+		this.texte = new Texte(texte);
+		this.add(this.texte);
+	}
+
+	public void changeTexte(String texte)
+	{
+		this.texte.changeTexte(texte);
+	}
+
+	protected abstract void clicGauche();
+	protected abstract void clicDroit();
+
+	public void mouseClicked(MouseEvent e)
+	{
+		if (SwingUtilities.isRightMouseButton(e))
+		{
+			this.clicDroit();
+		}
+		else
+		{
+			this.clicGauche();
+		}
+	}
+
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 }
