0899f0e3d9d41b3218a230e062b2cc81a38465dc
tiger008 <mythom008@gmail.com>
2015-02-18 12:35:02 +0100

    TP1 POnGL

--- POnGL/TP1/NReines/InterfaceIG.java
+++ POnGL/TP1/NReines/InterfaceIG.java
@@ -6,53 +6,53 @@
 /* Classe d'une fenêtre dans laquelle on peut insérer
    des éléments graphiques.
    Voir IG/Fenetre.java
- */
+   */
 
-interface Fenetre {
+interface Fenetre
+{
 
-    // Création
-    public Fenetre (String nom);
+	// Création
+	public Fenetre (String nom);
 
-    // Ajout d'un élément graphique
-    public void ajouteElement (JComponent element);
+	// Ajout d'un élément graphique
+	public void ajouteElement (JComponent element);
 
-    // Affichage de la fenêtre
-    public void dessineFenetre();
+	// Affichage de la fenêtre
+	public void dessineFenetre();
 
 }
 
 /* Classe permettant d'aligner des éléments graphiques
    sur une grille.
    Voir IG/Grille.java
-*/
+   */
 
-interface Grille {
-    
-    // Création
-    public Grille (int hauteur, int largeur);
+interface Grille
+{
+	// Création
+	public Grille (int hauteur, int largeur);
 
-    // Ajout d'éléments dans la grille
-    public void ajouteElement(JComponent element);
+	// Ajout d'éléments dans la grille
+	public void ajouteElement(JComponent element);
 
 }
 
 /* Classe d'un élément graphique sur lequel on peut
    cliquer pour déclencher des actions.
    Voir IG/ZoneCliquable.java
-*/
+   */
 
-interface ZoneCliquable {
-    
-    // Création d'une zone cliquable, avec ou sans texte.
-    public ZoneCliquable(int x, int y);
-    public ZoneCliquable(String texte, int x, int y);
+interface ZoneCliquable
+{
+	// Création d'une zone cliquable, avec ou sans texte.
+	public ZoneCliquable(int x, int y);
+	public ZoneCliquable(String texte, int x, int y);
 
-    // Actions à effectuer lors des clics.
-    public abstract void clicGauche();
-    public abstract void clicDroit();
+	// Actions à effectuer lors des clics.
+	public abstract void clicGauche();
+	public abstract void clicDroit();
 
-    // Modification de l'étiquette.
-    public void changeTexte(String texte);
-
+	// Modification de l'étiquette.
+	public void changeTexte(String texte);
 }
 
