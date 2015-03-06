d415c89090caf2188e13d3d0a358d9ed04a57bb9
tiger008 <mythom008@gmail.com>
2015-03-06 16:36:39 +0100

    Tp2 Ocaml

--- Ocaml/Tp2/jeu.ml
+++ Ocaml/Tp2/jeu.ml
@@ -9,22 +9,25 @@ let rec push x y r =
     if (mx - x) * (mx - x) + (my - y) * (my - y) < r * r then
         begin
             set_color red;
-            fill_circle x y r
+            fill_circle x y r;
+            drag x y x y r
         end
     else
         push x y r
-;;
-
-let rec jeu () =
-    let event = wait_next_event [Mouse_motion] in
-    let mx = event.mouse_x in
-    let my = event.mouse_y in
-    if push 100 100 10 then
-        Format.printf "%d %d@." mx my
+    and drag x y mx my r =
+        let event = wait_next_event [Mouse_motion;Button_up] in
+        if event.button == false then
+        begin
+            Format.printf "%d %d@." mx my;
+            push x y r
+        end
     else
-        jeu()
+        let mx = event.mouse_x in
+        let my = event.mouse_y in
+        clear_graph ();
+            fill_circle mx my r;
+            drag mx my mx my r
 ;;
 
-
-    fill_circle 100 100 10 ;;
-jeu () ;;
+fill_circle 100 100 10 ;;
+drag 100 100 100 100 10 ;;
