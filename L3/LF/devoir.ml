405e7e7385caf1b4756374e2e0cb2a7b0796e894
tiger008 <mythom008@gmail.com>
2016-03-23 17:29:56 +0100

    devoir lf modifie

--- L3/LF/devoir.ml
+++ L3/LF/devoir.ml
@@ -11,17 +11,18 @@ let rec contient_epsilon = function
     | Empty || Char(c) -> false
     | Or(r1, r2) -> contient_epsilon r1 || contient_epsilon r2
     | Concat(r1, r2) -> contient_epsilon r1 && contient_epsilon r2
-    | _ -> true (*Star ou Epsilon*)
+    | _ -> true (* Star ou Epsilon *)
 ;;
 
 let rec residu r c =
     match r with
     | Char(x) when x = c -> Epsilon
-    | Empty || Epsilon || Char(x) -> Empty
+    |  -> Empty
     | Or(r1, r2) -> Or((residu r1 c), (residu r2 c))
-    | Concat(r1, r2) when r1 != Epsilon -> Concat((residu r1 c), r2)
-    | Concat (_, r2) -> residu r2 c
-    | Star(r1) -> Star((residu r1 c))
+    | Concat(r1, r2) when (contient_epsilon r1) -> Or((residu r1 c), (residu r2 c))
+    | Concat(r1, r2) -> Concat(residu r1 c, r2)
+    | Star(r1) when (contient_epsilon (residu r1 c))-> r1 (* TODO : a modifier *)
+    | _ -> Empty (* Empty ou Epsilon ou Char(x) avec x != c *)
 ;;
 
 let rec reconnait r w =
