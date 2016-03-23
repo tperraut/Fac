fa0de99796c2c071880981ffff2386d446ee08ff
Thomas Perraut <tperrau@a-018431.ups.u-psud.fr>
2016-03-23 11:55:14 +0100

    TP2 réseau

---
+++ L3/Network/2TP/src/client_udp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define SIZE 100
+#define PORT 9600
+
+int	main(int argc, char **argv)
+{
+	return (0);
+}
---
+++ L3/Network/2TP/src/serveur_udp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include <string.h>
+#define PORT 9600
+
+int	main(int argc, char **argv)
+{
+	while (1)
+	{
+	}
+	return (0);
+}
