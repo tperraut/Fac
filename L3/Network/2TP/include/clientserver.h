c6c35b80a250447b9eaececde7718bc6eb953649
Thomas Perraut <tperrau@a-018439.ups.u-psud.fr>
2016-03-30 12:21:19 +0200

    TP2 reseau udp serv ok

---
+++ L3/Network/2TP/include/clientserver.h
@@ -0,0 +1,12 @@
+#ifndef CLIENTSERVER_H
+# define CLIENTSERVER_H
+# define PORT 9600
+# define INVALID_SOCKET -1
+# define SOCKET_ERROR -1
+# define BUF_SIZE 2048
+
+typedef int		SOCKET;
+typedef struct	sockaddre_in SOCKADDR_IN;
+typedef struct	sockaddr SOCKADDR;
+typedef struct	in_addr IN_ADDR;
+#endif
--- L3/Network/2TP/src/serveur_udp.c
+++ L3/Network/2TP/src/serveur_udp.c
@@ -4,14 +4,48 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
 #include <string.h>
-#define PORT 9600
+
+#include "clientserver.h"
 
 int	main(int argc, char **argv)
 {
-	while (1)
+	int			from_size;
+	char		buf[BUF_SIZE];
+	SOCKET		sockfd;
+	SOCKADDR_IN	sin;
+	SOCKADDR_IN	from = {0};
+
+	/*socket creation*/
+	if((sockfd = socket(AF_INET, SOCK_DGRAM, PF_INET))
+			== INVALID_SOCKET)
 	{
+		perror("socket()");
+		exit(errno);
 	}
+	/*initialize server*/
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = htonl(INADDR);
+	sin.sin_port = htons(PORT);
+	/*interface*/
+	if(bind(sockfd, (SOWKADDR *) &addr, sizeof(sin))
+			== SOCKET_ERROR)
+	{
+		perror("bind");
+		exit(errno);
+	}
+	from_size = sizeof(from);
+	while ((n = recvfrom(sockfd, buf, BUF_SIZE - 1, FLAGS, (SOCKEADDR *)&from,
+					&from_size)))
+	{
+		if(n < 0)
+		{
+			perror("recvfrom()");
+			exit(errno);
+		}
+		buf[n] = '\0';
+		write(1, buf, n);
+	}
+	close(sockfd);
 	return (0);
 }
