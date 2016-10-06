a08eb95128cfb613169a27d12a1e2b38f4bc2dea
tiger008 <mythom008@gmail.com>
2016-10-06 03:39:50 +0200

    testall more beautiful

--- M1/Compilation/2TP/testall.sh
+++ M1/Compilation/2TP/testall.sh
@@ -10,30 +10,27 @@ then
 fi
 for p in $@
 do
-	echo -n "FILE_NAME : "
-	echo $p
-	echo ""
-	echo -n "INPUT : "
+	echo -ne "\e[97;100mFILE_NAME\e[0m : "
+	echo -e "\033[1m$p\033[0m"
+	echo -ne "\e[97;100mINPUT\e[0m : "
 	cat $p
-	echo ""
-	echo -n "INTERPRETER : "
+	echo -ne "\e[97;100mINTERPRETER\e[0m : "
 	i=`./compilo -i $p`
-	echo $i
-	echo ""
-	echo -n "COMPILER : "
+	echo -e "\033[1m$i\033[0m"
+	echo -ne "\e[97;100mCOMPILER\e[0m : "
 	./compilo $p > test.asm
 	c=$(echo $(java -jar ../Mars4_5.jar test.asm) | awk '{print $NF}')
-	echo $c
+	echo -e "\033[1m$c\033[0m"
 	if [ "$i" != "$c" ]
 	then
-		echo -e "\033[31;1mFAIL\033[0m"
+		echo -e "\e[1;97;101m[FAIL]\e[0m"
 	else
-		echo -e "\033[32;1mOK\033[0m"
+		echo -e "\e[1;90;102m[OK]\e[0m"
 	fi
 	if [ $op = "1" ] || [ "$i" != "$c" ]
 	then
 		echo ""
-		echo "MIPS_CODE :"
+		echo -e "\e[97;100mMIPS_CODE\e[0m"
 		cat test.asm
 	fi
 	echo "###############################"
